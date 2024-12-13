ccb120800067e46814cbb77d21ef076a4bb334d3
hema <hemadandafara@gmail.com>
2024-12-13 20:12:06 +0700

    phpbaru

---
+++ kontak.php
@@ -0,0 +1,16 @@
+<h2>kontak</h2>
+<form action="simpan.php" method="post">
+<label>Nama</label>
+    <input type="text" name="nama" >
+    <br>
+    
+    <label>Email</label>
+    <input type="text" name="email" >
+    <br>
+
+    <label>Nomor HP</label>
+    <input type="tel" name="telp" >
+    <br>
+
+    <button type="submit">simpan</button>
+</form>
