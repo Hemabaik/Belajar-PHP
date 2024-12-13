2ed1d6b1c8804b3056395d46a157eed006c095ff
hema <hemadandafara@gmail.com>
2024-12-13 19:32:46 +0700

    php get

---
+++ pertemuansekian.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP GET and POST</title>
+</head>
+<body>
+    ini HTTP GET<br><br>
+    <form method="GET" action="">
+        <label>Nama</label>
+        <input type="text" name="telp">
+        <button type="submit">simpan</button>
+    </form>
+<br>
+    Ini HTTP POST<br><br>
+    <form method="POST" action="">
+        <label>Nama</label>
+        <input type="text" name="telp">
+        <button type="submit">simpan</button>
+    </form>
+
+</body>
+</html>
