b7af8883b6aa0e19d1537b5914b248627f444cba
hema <hemadandafara@gmail.com>
2024-12-20 17:12:20 +0700

    php

--- simpan.php
+++ simpan.php
@@ -1,16 +1,39 @@
-<?php
-if(isset($_POST['nama'])){
-    $nama   = $_POST['nama'];
-    $email  = $_POST['email'];
-    $telpon = $_POST['telp'];
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <titel>Belajar PHP-GET dan POST</title>
+</head>
+<body>
+    <from action="simpan.php" method="POST">
+        <label>Nama</label>
+        <input type="text" name="nama" placeholder="Masukan Nama" required>
+        <br>
+        <label>Email</label>
+        <input type="email" name="email" placeholder="Masukan Email" required>
+        <br>
+        <label>telepon</label>
+        <input type="text" name="telp" placeholder="Masukan Telepon" requider>
+        <br>
+        <butoon type="sumbit">Simpan Data</butoon>
+</from>
 
-    $data = "Nama: $nama Email: $email Telp: $telpon\n";
+<?php
+if (isset($_POST['nama']) && isset($_POST['email']) && isset($_POST['telp'])) {
+    $nama = $_POST['nama'];
+    $email = $_POST['email'];
+    $telepon = $_POST['telp'];
+
+    $data = "Nama: $nama, Email: $email, Telp: $telepon\n";
 
     $file = "kontak.txt";
-    file_put_contents($file, $data, FILE_APPEND);
-
-    echo "Data berhasil disimpan!";
-}else{
-
-    
+if (file_put_contents($file, $data, FILE_APPEND)) {
+    echo "<p style='color:green;'>Data Berhasil Disimpan</p>";
+ } else {
+    echo "<p style='color:red;'>Gagal menyimpan data</p>";
+ }
 }
+?>
+</body>
+</html>
