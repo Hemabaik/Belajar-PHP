c74c8cc12555669ca09081ea9c4d9e7a974d1f23
hema <hemadandafara@gmail.com>
2024-12-20 19:15:15 +0700

    login

--- proses.php
+++ proses.php
@@ -1,17 +1,25 @@
 <?php
+session_start();
+
 $pass = "hema123";
 $email = "hema@gmail.com";
 
-// Variabel input
-if (!empty($_POST['email']) && !empty($_POST['password'])) {
-if ($_POST['email'] === $email && $_POST['password'] === $pass) {
-  header("Location: simpan.php");
-  exit();
- } else {
-   header("Location: login.php");
-   exit();
- }
+if (isset($_POST['email']) && isset($_POST['password'])) {
+    if ($_POST['email'] == $email) {
+        if ($_POST['password'] == $pass) {
+            $_SESSION['email'] = $email;
+            header("Location: simpan.php");
+            exit();
+        } else {
+            header("Location: login.php?status=gagal");
+            exit();
+        }
+    } else {
+        header("Location: login.php?status=gagal");
+        exit();
+    }
 } else {
-   header("Location: login.php");
-exit();
+    header("Location: login.php?status=gagal");
+    exit();
 }
+?>
